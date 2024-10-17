0c222661417334a72901402805c1d67f06df9c37
yuanshibo910 <yuanshibo910@163.com>
2024-10-17 17:09:20 +0800

    初始化按钮组件

---
+++ src/components/Button/types.ts
@@ -0,0 +1,9 @@
+/*
+ *Button属性类型
+ *  type(样式) Primary Danger Info Success Warning
+ *  plain 展现模式 boolean
+ *  round 圆角 boolean
+ *  circle 原型按钮 boolean
+ *  size 按钮大小 small normal large
+ *  disabled 禁用 boolean
+ */
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+// import eslint from 'vite-plugin-eslint';
 
 import path from 'path';
 
