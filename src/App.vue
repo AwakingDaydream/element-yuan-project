4e7049a3145a4ec2d50b21f4dc3895221d3be072
yuanshibo910 <yuanshibo910@163.com>
2024-10-18 21:28:40 +0800

    完成button组件的编写

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,36 @@ import vkButton from '@/components/Button/Button.vue';
 </script>
 
 <template>
-	<h3>基本</h3>
-	<div class="123">
+	<h3>type</h3>
+	<div>
 		<vk-button>vk-button</vk-button>
-		<vk-button circle>vk-button</vk-button>
-		<vk-button round>vk-button</vk-button>
 		<vk-button type="primary">vk-button</vk-button>
 		<vk-button type="warning">vk-button</vk-button>
 		<vk-button type="success">vk-button</vk-button>
 		<vk-button type="danger">vk-button</vk-button>
 		<vk-button type="info">vk-button</vk-button>
 	</div>
-	<h3>朴素</h3>
+	<h3>plain</h3>
 	<div>
 		<vk-button plain>vk-button</vk-button>
-		<vk-button circle plain>vk-button</vk-button>
-		<vk-button round plain>vk-button</vk-button>
 		<vk-button type="primary" plain>vk-button</vk-button>
 		<vk-button type="warning" plain>vk-button</vk-button>
 		<vk-button type="success" plain>vk-button</vk-button>
 		<vk-button type="danger" plain>vk-button</vk-button>
 		<vk-button type="info" plain>vk-button</vk-button>
 	</div>
-	<h3>禁用</h3>
+	<h3>round</h3>
+	<div>
+		<vk-button round>vk-button</vk-button>
+		<vk-button type="primary" round>vk-button</vk-button>
+		<vk-button type="warning" round>vk-button</vk-button>
+		<vk-button type="success" round>vk-button</vk-button>
+		<vk-button type="danger" round>vk-button</vk-button>
+		<vk-button type="info" round>vk-button</vk-button>
+	</div>
+	<h3>disabled</h3>
 	<div>
 		<vk-button disabled>vk-button</vk-button>
-		<vk-button circle disabled>vk-button</vk-button>
-		<vk-button round disabled>vk-button</vk-button>
 		<vk-button type="primary" disabled>vk-button</vk-button>
 		<vk-button type="warning" disabled>vk-button</vk-button>
 		<vk-button type="success" disabled>vk-button</vk-button>
