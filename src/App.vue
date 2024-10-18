7cfa54fc6c2adef4299935849443b1e4dfc6c77e
yuanshibo910 <yuanshibo910@163.com>
2024-10-18 17:46:55 +0800

    完善按钮组件代码，遗留问题：scss文件中root 无法执行mix相关color混合函数

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <script setup lang="ts">
-import Button from '@/components/Button/Button.vue';
+import vkButton from '@/components/Button/Button.vue';
 </script>
 
 <template>
-	<header>
-		<Button type="success" plain round circle size="large" autofocus>vk-button</Button>
-	</header>
-
-	<main></main>
+	<h3>基本</h3>
+	<div class="123">
+		<vk-button>vk-button</vk-button>
+		<vk-button circle>vk-button</vk-button>
+		<vk-button round>vk-button</vk-button>
+		<vk-button type="primary">vk-button</vk-button>
+		<vk-button type="warning">vk-button</vk-button>
+		<vk-button type="success">vk-button</vk-button>
+		<vk-button type="danger">vk-button</vk-button>
+		<vk-button type="info">vk-button</vk-button>
+	</div>
+	<h3>朴素</h3>
+	<div>
+		<vk-button plain>vk-button</vk-button>
+		<vk-button circle plain>vk-button</vk-button>
+		<vk-button round plain>vk-button</vk-button>
+		<vk-button type="primary" plain>vk-button</vk-button>
+		<vk-button type="warning" plain>vk-button</vk-button>
+		<vk-button type="success" plain>vk-button</vk-button>
+		<vk-button type="danger" plain>vk-button</vk-button>
+		<vk-button type="info" plain>vk-button</vk-button>
+	</div>
+	<h3>禁用</h3>
+	<div>
+		<vk-button disabled>vk-button</vk-button>
+		<vk-button circle disabled>vk-button</vk-button>
+		<vk-button round disabled>vk-button</vk-button>
+		<vk-button type="primary" disabled>vk-button</vk-button>
+		<vk-button type="warning" disabled>vk-button</vk-button>
+		<vk-button type="success" disabled>vk-button</vk-button>
+		<vk-button type="danger" disabled>vk-button</vk-button>
+		<vk-button type="info" disabled>vk-button</vk-button>
+	</div>
 </template>
 
-<style scoped lang="scss">
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		@include mo;
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-}
-</style>
+<style scoped lang="scss"></style>
