16177c8656e1df444076845ffdad16bb97224647
yuanshibo910 <yuanshibo910@163.com>
2024-12-03 13:25:33 +0800

    部署操作

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { componentPreview, containerPreview } from '@vitepress-demo-preview/plug
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-	base: '/blog/',
+	base: '/element-yuan-project/',
 	title: 'ChinaYuan Project',
 	description: 'A VitePress Site',
 	vite: {
